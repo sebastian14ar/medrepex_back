fe07412ac8dd5689b8bf2eb834ae04f988ec19fb
MiguelAValdesG <mvaldes988@gmail.com>
2020-03-28 18:12:22 -0600

    Refactorización, se agrega el folder 'routes'

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 import express from "express";
-import nodemailer from "nodemailer";
 import cors from "cors";
 
 const app = express();
 const https = require('https');
 const fs = require('fs');
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.medrepexpress.com",
-  port: 587,
-  auth: {
-    user: "sales@medrepexpress.com",
-    pass: "mxCY55Fc"
-  }
-});
+const medRep = require("./routes/medrep");
+const indec = require("./routes/indec");
 
 /* ==================================================================================================
 ======================================= SSL Configurations ==========================================
@@ -29,7 +21,6 @@ cert: certificate,
 ca: ca
 };
 
-
 /* ==================================================================================================
 =========================================== Middleware ==============================================
 ===================================================================================================*/
@@ -37,6 +28,8 @@ ca: ca
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', medRep);
+app.use('/indec', indec);
 
 // Configure headers and cors
 app.use((req, res, next) => {
@@ -54,245 +47,9 @@ app.use((req, res, next) => {
 ============================================== Routes ===============================================
 ===================================================================================================*/
 
-// ----------------------------------------- CONTACT US ---------------------------------------------
-app.post("/api/contact-us", async (req, res) => {
-  console.log(req.body);
-
-  var htmlMail = `
-        <h3>----- CONTACT US -----</h3>
-        <h3>INFORMATION OF CLIENT: </h3>
-        <h4>
-            <p> Name: ${req.body.name}
-            <p> Email Address:  ${req.body.email}
-        </h4>
-        <h3>SUBJECT: </h3>
-        <h4>
-            <p> ${req.body.subject}
-        </h4>
-        <h3>MESSAGE: </h3>
-        <h4>
-            <p> ${req.body.message}
-        </h4>
-         `;
-
-  var subject = "CONTACT US | " + req.body.name;
-
-  var mailOptions = {
-    from: req.body.email,
-    // to: "sitioweb@indecmexico.com",
-    to: "sales@medrepexpress.com",
-    subject: subject,
-    html: htmlMail
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      res.send(error);
-    } else {
-      console.log("Email sent");
-      res.send("Email sent");
-    }
-  });
-});
-
-// -------------------------------------- REQUEST SAMPLES -------------------------------------------
-app.post("/api/req-samples", async (req, res) => {
-  console.log(req.body);
-
-  var htmlMail = `
-        <h3>----- REQUEST SAMPLES -----</h3>
-        <h3>INFORMATION OF CLIENT: </h3>
-        <h4>
-            <p> Business Name: ${req.body.businessName}
-            <p> Requestor's Name: ${req.body.name}
-            <p> Phone Number: ${req.body.phone}
-            <p> Fax: ${req.body.fax}
-            <p> Email: ${req.body.email}
-            <p> Shipping Address: ${req.body.shippingAddress}
-        </h4>
-        <h3>PRODUCT APPLICATION: </h3>
-        <h4>
-            <p> ${req.body.productApp}
-        </h4>
-    `;
-
-  var subject =
-    "REQUEST SAMPLES | " + req.body.businessName + " | " + req.body.name;
-
-  var mailOptions = {
-    from: req.body.email,
-    // to: "sitioweb@indecmexico.com",
-    to: "sales@medrepexpress.com",
-    subject: subject,
-    html: htmlMail
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      res.send(error);
-    } else {
-      console.log("Email sent");
-      res.send("Email sent");
-    }
-  });
-});
-
-// ------------------------------------ REQUEST PRODUCT INFO ----------------------------------------
-app.post("/api/req-product-info", async (req, res) => {
-  console.log(req.body);
-
-  var htmlMail = `
-        <h3>----- REQUEST PRODUCT INFO -----</h3>
-        <h3>INFORMATION OF CLIENT: </h3>
-        <h4>
-            <p> Business Name: ${req.body.businessName}
-            <p> Requestor's Name: ${req.body.name}
-            <p> Phone Number: ${req.body.phone}
-            <p> Fax: ${req.body.fax}
-            <p> Email: ${req.body.email}
-            <p> Shipping Address: ${req.body.shippingAddress}
-        </h4>
-        <h3>PRODUCT INQUIRY: </h3>
-        <h4>
-            <p> ${req.body.productInq}
-        </h4>
-    `;
-
-  var subject =
-    "REQUEST PRODUCT INFO | " + req.body.businessName + " | " + req.body.name;
-
-  var mailOptions = {
-    from: req.body.email,
-    // to: "sitioweb@indecmexico.com",
-    to: "sales@medrepexpress.com",
-    subject: subject,
-    html: htmlMail
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      res.send(error);
-    } else {
-      console.log("Email sent");
-      res.send("Email sent");
-    }
-  });
-});
-
-// --------------------------------------- SHOPPING CART --------------------------------------------
-app.post("/api/shopping-cart", async (req, res) => {
-  var purchaseOrder = "";
-  var total = 0;
-  for (var key in req.body.items) {
-    if (req.body.items.hasOwnProperty(key)) {
-      var itemTotal = Math.round((req.body.items[key].cant * req.body.items[key].price) * 100) / 100;
-      total += itemTotal;
-      purchaseOrder +=
-        req.body.items[key].code + " | "  +
-        req.body.items[key].description + " | "  +
-        req.body.items[key].cant + " | $"  +
-        req.body.items[key].price + " | $"  +
-        itemTotal +
-        "<p>";
-    }
-  }  
-  total = Math.round((total + 9.9) * 100) / 100;
-  purchaseOrder += "STANDAR GROUND SHIPPING = $9.90" + "<p>";
-  purchaseOrder += "<p>" + "<p>" + "TOTAL = $" + total;
-
-  var htmlMail = `
-        <h1>----- PURCHASE ORDER -----</h1>
-        <h2>BILL TO: </h2>
-          <p> Business/Company Name: ${req.body.businessName}
-          <p> Address: ${req.body.address}
-          <p> City: ${req.body.city}
-          <p> State/Province: ${req.body.state}
-          <p> ZIP Code: ${req.body.zipCode}
-          <p> Phone Number: ${req.body.phone}
-          <p> Email: ${req.body.email}
-        <h2>PAYMENT (CREDIT CARD): </h2>
-          <p> Credit Card Name Holder: ${req.body.cardNameHolder}
-          <p> Credit Card Number: ${req.body.numberCard}
-          <p> Expiration Date: ${req.body.numberExpDate}
-        <h2>SHIP TO: </h2>
-          <p> Business/Company Name: ${req.body.businessName_ship}          
-          <p> Address: ${req.body.address_ship}
-          <p> City: ${req.body.city_ship}
-          <p> State/Province: ${req.body.state_ship}
-          <p> ZIP Code: ${req.body.zipCode_ship}
-          <p> Phone Number: ${req.body.phone_ship}
-          <p> Email: ${req.body.email_ship}
-        <h2>PURCHASE ORDER: </h2>
-          <p> ITEM | DESCRIPTION | QUANTITY | PRICE | TOTAL
-          <p>${purchaseOrder}
-    `;
-
-  var subject =
-    "PURCHASE ORDER | " + req.body.businessName + " | " + req.body.firstName;
-
-  var mailOptions = {
-    from: req.body.email,
-    // to: "sitioweb@indecmexico.com",
-    to: "sales@medrepexpress.com",
-    subject: subject,
-    html: htmlMail
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-      res.send(error);
-    } else {
-      console.log("Email sent");
-      res.send("Email sent");
-    }
-  });
-});
-
-// -------------------------------------- CONTACT US INDEC ------------------------------------------
-app.post('/indec/contact-us', async(req, res) => {
-
-  console.log(req.body);
-
-  if(req.body.name == null)
-    res.send('Ayudanos agregando tu nombre, queremos conocerte');
-  else if(req.body.email == null)  
-    res.send('Necesitamos tu dirección de correo para contactarte')
-  else {
-    var htmlMail = `
-      <h3>INFORMACIÓN DEL CLIENTE: </h3>
-      <h4>
-        <p> Nombre: ${req.body.name}
-        <p> Ciudad: ${req.body.city} 
-        <p> Dirección de correo electónico: ${req.body.email}
-      </h4>
-      <h3>MENSAJE: </h3>
-      <h4> 
-        <p>${req.body.message}
-      </h4>
-      `;
-    
-    var subject = 'Me gustaría distribuir con ustedes | ' +  req.body.name + ' | ' + req.body.city;
-
-    var mailOptions = {
-      from: req.body.txtEmail,
-      to: 'ventas@indecmexico.com, carlos.alvarado@indecmexico.com',
-      subject: subject,
-      html: htmlMail
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        res.send(error);
-      }
-      else {
-        res.send('EL correo se envio con exito');
-      }
-    });
-  }
+app.get('/', function (req, res) {
+  res.writeHead(200);
+  res.end("by VainillaDev\n");
 });
 
 /* ==================================================================================================
@@ -302,14 +59,8 @@ app.post('/indec/contact-us', async(req, res) => {
 const port = process.env.port || 3001;
 https.createServer(credentials, app).listen(port, () => {
   console.log("Listening on port: " + port);
-});
-
-
-/* ============================================= TEST ============================================*/
-
-app.get('/', function (req, res) {
-  res.writeHead(200);
-  res.end("by VainillaDev\n");
+// app.listen(port, () => {
+//   console.log("Listenig on port: " + port);
 });
 
 /* EXECUTION COMMAND
---
+++ routes/medrep.js
@@ -0,0 +1,212 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const router = express.Router();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.medrepexpress.com",
+  port: 587,
+  auth: {
+    user: "sales@medrepexpress.com",
+    pass: "mxCY55Fc"
+  }
+});
+
+// ----------------------------------------- CONTACT US ---------------------------------------------
+router.post("/contact-us", async (req, res) => {
+
+  const htmlMail = `
+        <h3>----- CONTACT US -----</h3>
+        <h3>INFORMATION OF CLIENT: </h3>
+        <h4>
+            <p> Name: ${req.body.name}
+            <p> Email Address:  ${req.body.email}
+        </h4>
+        <h3>SUBJECT: </h3>
+        <h4>
+            <p> ${req.body.subject}
+        </h4>
+        <h3>MESSAGE: </h3>
+        <h4>
+            <p> ${req.body.message}
+        </h4>
+         `;
+
+  const subject = "CONTACT US | " + req.body.name;
+  
+  var mailOptions = {
+    from: req.body.email,
+    // to: "sitioweb@indecmexico.com",
+    to: "sales@medrepexpress.com",
+    subject: subject,
+    html: htmlMail
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      console.log("Email sent");
+      res.send("Email sent");
+    }
+  });
+});
+
+// -------------------------------------- REQUEST SAMPLES -------------------------------------------
+router.post("/req-samples", async (req, res) => {
+
+  const htmlMail = `
+        <h3>----- REQUEST SAMPLES -----</h3>
+        <h3>INFORMATION OF CLIENT: </h3>
+        <h4>
+            <p> Business Name: ${req.body.businessName}
+            <p> Requestor's Name: ${req.body.name}
+            <p> Phone Number: ${req.body.phone}
+            <p> Fax: ${req.body.fax}
+            <p> Email: ${req.body.email}
+            <p> Shipping Address: ${req.body.shippingAddress}
+        </h4>
+        <h3>PRODUCT APPLICATION: </h3>
+        <h4>
+            <p> ${req.body.productApp}
+        </h4>
+    `;
+
+  const subject = "REQUEST SAMPLES | " + req.body.businessName + " | " + req.body.name;
+  
+  var mailOptions = {
+    from: req.body.email,
+    // to: "sitioweb@indecmexico.com",
+    to: "sales@medrepexpress.com",
+    subject: subject,
+    html: htmlMail
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      console.log("Email sent");
+      res.send("Email sent");
+    }
+  });
+
+});
+
+// ------------------------------------ REQUEST PRODUCT INFO ----------------------------------------
+router.post("/req-product-info", async (req, res) => {
+
+  const htmlMail = `
+        <h3>----- REQUEST PRODUCT INFO -----</h3>
+        <h3>INFORMATION OF CLIENT: </h3>
+        <h4>
+            <p> Business Name: ${req.body.businessName}
+            <p> Requestor's Name: ${req.body.name}
+            <p> Phone Number: ${req.body.phone}
+            <p> Fax: ${req.body.fax}
+            <p> Email: ${req.body.email}
+            <p> Shipping Address: ${req.body.shippingAddress}
+        </h4>
+        <h3>PRODUCT INQUIRY: </h3>
+        <h4>
+            <p> ${req.body.productInq}
+        </h4>
+    `;
+
+  const subject = "REQUEST PRODUCT INFO | " + req.body.businessName + " | " + req.body.name;
+
+  var mailOptions = {
+    from: req.body.email,
+    // to: "sitioweb@indecmexico.com",
+    to: "sales@medrepexpress.com",
+    subject: subject,
+    html: htmlMail
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      console.log("Email sent");
+      res.send("Email sent");
+    }
+  });
+
+});
+
+// --------------------------------------- SHOPPING CART --------------------------------------------
+router.post("/shopping-cart", async (req, res) => {
+
+  var purchaseOrder = "";
+  var total = 0;
+  for (var key in req.body.items) {
+    if (req.body.items.hasOwnProperty(key)) {
+      var itemTotal = Math.round((req.body.items[key].cant * req.body.items[key].price) * 100) / 100;
+      total += itemTotal;
+      purchaseOrder +=
+        req.body.items[key].code + " | "  +
+        req.body.items[key].description + " | "  +
+        req.body.items[key].cant + " | $"  +
+        req.body.items[key].price + " | $"  +
+        itemTotal +
+        "<p>";
+    }
+  }  
+  total = Math.round((total + 9.9) * 100) / 100;
+  purchaseOrder += "STANDAR GROUND SHIPPING = $9.90" + "<p>";
+  purchaseOrder += "<p>" + "<p>" + "TOTAL = $" + total;
+
+  const htmlMail = `
+        <h1>----- PURCHASE ORDER -----</h1>
+        <h2>BILL TO: </h2>
+          <p> Business/Company Name: ${req.body.businessName}
+          <p> Address: ${req.body.address}
+          <p> City: ${req.body.city}
+          <p> State/Province: ${req.body.state}
+          <p> ZIP Code: ${req.body.zipCode}
+          <p> Phone Number: ${req.body.phone}
+          <p> Email: ${req.body.email}
+        <h2>PAYMENT (CREDIT CARD): </h2>
+          <p> Credit Card Name Holder: ${req.body.cardNameHolder}
+          <p> Credit Card Number: ${req.body.numberCard}
+          <p> Expiration Date: ${req.body.numberExpDate}
+        <h2>SHIP TO: </h2>
+          <p> Business/Company Name: ${req.body.businessName_ship}          
+          <p> Address: ${req.body.address_ship}
+          <p> City: ${req.body.city_ship}
+          <p> State/Province: ${req.body.state_ship}
+          <p> ZIP Code: ${req.body.zipCode_ship}
+          <p> Phone Number: ${req.body.phone_ship}
+          <p> Email: ${req.body.email_ship}
+        <h2>PURCHASE ORDER: </h2>
+          <p> ITEM | DESCRIPTION | QUANTITY | PRICE | TOTAL
+          <p>${purchaseOrder}
+    `;
+
+  const subject = "PURCHASE ORDER | " + req.body.businessName + " | " + req.body.city;
+
+  var mailOptions = {
+    from: req.body.email,
+    // to: "sitioweb@indecmexico.com",
+    to: "sales@medrepexpress.com",
+    subject: subject,
+    html: htmlMail
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      console.log("Email sent");
+      res.send("Email sent");
+    }
+  });
+
+});
+
+
+module.exports = router;
